bec8d3f8f14299c0cbebd5c6ca899d802d621927
Grayson-choi <784wldnd@gmail.com>
2021-01-05 23:36:05 +0900

    expense_tracker js 코드 추가

--- meal_finder/script.js
+++ meal_finder/script.js
@@ -55,7 +55,6 @@ function getMealById(mealID) {
   fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
     .then(res => res.json())
     .then(data => {
-
       const meal = data.meals[0];
 
       addMealToDOM(meal);
