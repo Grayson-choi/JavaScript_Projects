d23c8a2b3d96b0e7f0dfcc20fd4f9ce07fdfe298
Grayson-choi <784wldnd@gmail.com>
2021-02-19 22:45:13 +0900

    speak_number guessing game 완성

--- speak_number_guessing_game/script.js
+++ speak_number_guessing_game/script.js
@@ -15,8 +15,49 @@ recognition.start();
 function onSpeak(e) {
   const msg = e.results[0][0].transcript;
 
-  // writeMessage(msg);
+  writeMessage(msg);
+  checkNumber(msg);
   // console.log(msg);
+}
+
+// Write what user speaks
+function writeMessage(msg) {
+  msgEl.innerHTML = `
+    <div> You said: </div>
+    <span class="box">${msg}</span>
+  `;
+}
+
+// Check msg against number
+function checkNumber(msg) {
+  const num = +msg; // 문자를 숫자로 바꾸는 것
+
+  // Check if valid number
+  if(Number.isNaN(num)) {
+    msgEl.innerHTML += '<div>That is not a valid number</div>';
+    return;
+  }
+
+  // Check in range
+  if(num > 100 || num < 1) {
+    msgEl.innerHTML += '<div>Number must be between 1 and 100</div>';
+    return;
+  }
+
+  // Check number
+  if(num === randomNum) {
+    document.body.innerHTML = `
+    <h2>Congrats! You have guessed the number! <br><br>
+    It was ${num}</h2>
+    <button class="play-again" id="play-again">Play Again</button>
+    </h2>
+    `;
+  }else if(num > randomNum) {
+    msgEl.innerHTML += '<div>GO LOWER</div>';
+  }else {
+    msgEl.innerHTML += '<div>Go Higher</div>';
+  }
+
 }
 
 // Generate random number
@@ -27,3 +68,11 @@ function getRandomNumber() {
 // Speak result
 recognition.addEventListener('result', onSpeak);
 
+// End SR service
+recognition.addEventListener('end', () => recognition.start());
+
+document.body.addEventListener('click', (e) => {
+  if (e.target.id == 'play-again') {
+    window.location.reload();
+  }
+})
