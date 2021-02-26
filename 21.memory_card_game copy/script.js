ef5adb49538c5f0fc028ae21ab5f21518af39a06
Grayson-choi <784wldnd@gmail.com>
2021-02-26 13:50:16 +0900

    memory_card_game 수정

---
+++ 21.memory_card_game copy/script.js
@@ -0,0 +1,132 @@
+const rulesBtn = document.getElementById('rules-btn');
+const closeBtn = document.getElementById('close-btn');
+const rules = document.getElementById('rules');
+const startBtn = document.getElementById('start-btn');
+const card = document.getElementsByName('card');
+const hintBtn = document.getElementById('hint-btn');
+let cnt = 0;
+let selectedId = "";
+
+function startGame() {
+  window.location.reload();
+}
+
+function clickCard() {
+  console.log("cnt", cnt);
+  console.log(selectedId);
+  this.classList.toggle('clicked');
+  this.classList.toggle('opened');
+  console.log(this.className);
+  console.log(this.id);
+  
+  if (this.classList.contains('opened')){
+    this.innerHTML =`
+    <img src="img/${this.id}.jpg">
+  `;
+  } else {
+    this.innerHTML = ""
+  }
+
+  if (selectedId){
+  // 정답을 맞췄을 때
+  if (this.id == selectedId ){
+    console.log("right");
+    setTimeout(function() {
+      selectedEl = document.querySelectorAll(".clicked");
+      console.log(selectedEl);
+      for (let i = 0; i < selectedEl.length; i++){
+        selectedEl[i].classList.replace("clicked", "correct")
+      }
+      selectedId = "";
+    }, 500);
+    cards = document.querySelectorAll(".correct");
+    console.log("cards", cards);
+
+    cnt += 1;
+    if (cards.length == 20){
+      document.body.innerHTML = `
+      <h2>축하합니다. 게임을 성공하셨습니다.<br><br>
+      ${cnt}번 만에 성공하셨습니다.
+      </h2>
+
+      `
+    }
+
+  } else if(this.id != selectedId){
+    console.log("wrong");
+    setTimeout(function() {
+    selectedEl = document.querySelectorAll(".clicked");
+    console.log(selectedEl);
+    for (let i = 0; i < selectedEl.length; i++){
+      selectedEl[i].classList.remove("clicked");
+      selectedEl[i].classList.remove("opened");
+      selectedEl[i].innerHTML = "";
+    }
+    selectedId = "";
+  }, 700);
+    cnt += 1;
+  } 
+} else {
+  selectedId = this.id
+}
+  
+
+
+}
+
+
+cardList = [];
+for(j = 0; j < 2; j++){
+for(i = 1; i< 11; i ++){
+  let card = document.createElement('div');
+  card.classList.add('card');
+  card.id = `${i}`;
+  
+  card.innerHTML = `
+  <div class="inner-card">
+  <div class="inner-card-front">
+    <p>
+      1
+    </p>
+  </div>
+  <div class="inner-card-back">
+    <img src=img/${i}.jpg>
+  </div>
+</div>
+  `;
+
+  card.addEventListener('click', clickCard);
+  cardList.push(card);
+  
+  }
+}
+
+cardList.sort(function(){
+  return Math.random() - Math.random();
+})
+
+for (i = 0; i < cardList.length; i++) {
+  cards.appendChild(cardList[i]);  
+}
+
+
+function hintGame(){
+  console.log("hint Game");
+  closeCards = document.querySelectorAll(".card");
+  
+  for (let i = 0; i < closeCards.length; i++){
+    if(!(closeCards[i].classList.contains("correct"))){
+      closeCards[i].classList.add("opened");
+    }
+  }
+  setTimeout(function() {
+    console.log('Works!');
+  }, 2000);
+}
+
+
+// Rules and close event handlers
+rulesBtn.addEventListener('click', () => rules.classList.add('show'));
+closeBtn.addEventListener('click', () => rules.classList.remove('show'));
+startBtn.addEventListener('click', startGame);
+hintBtn.addEventListener('click', hintGame);
